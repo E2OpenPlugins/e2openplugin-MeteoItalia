d88527cb1fa3eacc68f5cecd93f4e2c2037b958e
meo <lupomeo@gmail.com>
2011-10-09 19:54:50 +0200

    setup.py: add missed .png files

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ setup (name = 'enigma2-plugin-extensions-meteoitalia',
        author_email='lupomeo@hotmail.com',
        packages = [pkg],
        package_dir = {pkg: 'plugin'}
+       package_data={pkg: ['*.png']}
       )
