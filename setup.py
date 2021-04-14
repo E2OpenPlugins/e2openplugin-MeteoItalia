3307e58c1f8cfbab3c0fb62d0bae4b3b26826914
persianpros <persianpros@yahoo.com>
2021-04-14 15:38:55 +0430

    PEP8 double aggressive E251 and E252

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup, Extension
 
 pkg = 'Extensions.MeteoItalia'
-setup (name = 'enigma2-plugin-extensions-meteoitalia',
-       version = '0.1',
+setup (name='enigma2-plugin-extensions-meteoitalia',
+       version='0.1',
        license='GPLv2',
        url='https://github.com/E2OpenPlugins',
        description='MeteoItalia',
        long_description='Weather Forecast Italy',
        author='meo',
        author_email='lupomeo@hotmail.com',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
+       packages=[pkg],
+       package_dir={pkg: 'plugin'},
        package_data={pkg: ['*.png']}
       )
